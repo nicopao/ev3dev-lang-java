3a393b4a8ff616102defe38120b5af9c5542bb2e
Paoletti Nicola UFAC002 <nicola.paoletti@rhul.ac.uk>
2021-11-02 22:04:29 +0000

    Fixed NXTSoundSensor normalization

---
+++ src/main/java/ev3dev/sensors/nxt/NXTSensorConstants.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package ev3dev.sensors.nxt;
+
+/**
+ * Constants useful for handling NXT sensors
+ * 
+ * @author nicopao
+ *
+ */
+public interface NXTSensorConstants {
+    public static final float ADC_REF = 5.0f; // 5.0 Volts
+	public static final int ADC_RES = 1023;
+}
--- src/main/java/ev3dev/sensors/nxt/NXTSoundSensor.java
+++ src/main/java/ev3dev/sensors/nxt/NXTSoundSensor.java
@@ -1,63 +1,60 @@
 package ev3dev.sensors.nxt;
 
+import java.io.File;
+
 import ev3dev.sensors.BaseSensor;
 import ev3dev.sensors.GenericMode;
 import ev3dev.utils.Sysfs;
-import java.io.File;
 import lejos.hardware.port.Port;
 import lejos.hardware.sensor.SensorMode;
 import lejos.robotics.SampleProvider;
 
 public class NXTSoundSensor extends BaseSensor {
 
-    private static final String LEGO_NXT_SOUND = "lego_nxt_sound";
-    private static final String DBMODE = "NXT-SOUND-DB";
-    private static final String DBAMODE = "NXT-SOUND-DBA";
+	private static final String LEGO_NXT_SOUND = "lego_nxt_sound";
+	private static final String DBMODE = "NXT-SOUND-DB";
+	private static final String DBAMODE = "NXT-SOUND-DBA";
 
-    /**
-     * Constructor
-     *
-     * @param port Port name 
-     */
-    public NXTSoundSensor(Port port) {
+	/**
+	 * Constructor
+	 *
+	 * @param port Port name
+	 */
+	public NXTSoundSensor(Port port) {
 
-        super(port, LEGO_I2C, LEGO_NXT_SOUND);
-    
-        setModes(new SensorMode[] {
-            new InternalMode(this.PATH_DEVICE, 1, "DB"),
-            new InternalMode(this.PATH_DEVICE, 1, "DBA")
-        });
+		super(port, LEGO_I2C, LEGO_NXT_SOUND);
 
-    }
-  
-    public SampleProvider getDBMode() {
-        switchMode(DBMODE, SWITCH_DELAY);
-        return getMode(0);
-    }
-  
-    public SampleProvider getDBAMode() {
-        switchMode(DBAMODE, SWITCH_DELAY);
-        return getMode(1);
-    }
-  
-    private class InternalMode extends GenericMode {
-        private File devicePath;
+		setModes(new SensorMode[] { new InternalMode(this.PATH_DEVICE, 1, "DB"),
+				new InternalMode(this.PATH_DEVICE, 1, "DBA") });
 
-        public InternalMode(File pathDevice, int sampleSize, String modeName) {
-            super(pathDevice, sampleSize, modeName);
-            this.devicePath = pathDevice;
-        }
+	}
 
-    
-        @Override
-        public void fetchSample(float[] sample, int offset) {
-            float reading = Sysfs.readFloat(this.devicePath + "/" + "value0");
-            reading = reading / 10.0f;
-            sample[offset] = reading;
-        }
+	public SampleProvider getDBMode() {
+		switchMode(DBMODE, SWITCH_DELAY);
+		return getMode(0);
+	}
 
-    }
+	public SampleProvider getDBAMode() {
+		switchMode(DBAMODE, SWITCH_DELAY);
+		return getMode(1);
+	}
+
+	private class InternalMode extends GenericMode {
+		private File devicePath;
+
+		public InternalMode(File pathDevice, int sampleSize, String modeName) {
+			super(pathDevice, sampleSize, modeName);
+			this.devicePath = pathDevice;
+		}
+
+		@Override
+		public void fetchSample(float[] sample, int offset) {
+			float reading = Sysfs.readFloat(this.devicePath + "/" + "value0");
+//            reading = reading / 10.0f;
+			reading = 1.0f - (reading / NXTSensorConstants.ADC_REF / (float) NXTSensorConstants.ADC_RES);
+			sample[offset] = reading;
+		}
+
+	}
 
 }
-
-
